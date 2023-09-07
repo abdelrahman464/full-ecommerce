3d889e753d24c7d5ae66956a735fe6864803b10f
Abd_El Rahman Mohamed <apdomedo6@gmail.com>
2023-09-07 17:09:17 +0300

    convert size to array of string insted of array of numbers

--- models/productModel.js
+++ models/productModel.js
@@ -58,7 +58,7 @@ const productSchema = new mongoose.Schema(
       type: Number,
     },
     colors: [String],
-    sizes: [Number],
+    sizes: [String],
     imageCover: {
       type: String,
       required: [true, "Product image cover is required"],
